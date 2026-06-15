d05087ab524b106a27e6f862af89d465375548fb
pintilieg <geaniionut.pintilie@previnet.it>
2026-06-15 14:31:40 +0200

    prime animazioni

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,25 @@
-import { pages } from "./UI"
+import { pageAtom, pages } from "./UI"
 import Page from "./Page"
+import { useAtom } from "jotai"
 
-const Book = ({...props}) => {
-    return(
-        <group {...props}>
+const Book = ({ ...props }) => {
+
+    const [page] = useAtom(pageAtom)
+
+    return (
+        <group {...props} rotation-y={-Math.PI / 2}>
             {
                 [...pages].map((pageData, index) => (
-                    index === 0 
-                    ? (
-                    <Page key={index} number={index} {...pageData} />
-                    ) 
-                    :  null
-                ))
-            }
+                    <Page 
+                    key={index} 
+                    number={index} 
+                    page = {page}
+                    {...pageData} 
+                    opened = {page > index}
+                    bookClosed={page === 0 || page === pages.length}
+                    />
+                )
+            )}
         </group>
     )
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,9 @@
+import { useHelper, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react"
-import { BoxGeometry, Uint16BufferAttribute, Vector3, Float32BufferAttribute, Bone, Skeleton, SkinnedMesh, Color, MeshStandardMaterial } from "three";
+import { BoxGeometry, Uint16BufferAttribute, Vector3, Float32BufferAttribute, Bone, Skeleton, SkinnedMesh, Color, MeshStandardMaterial, SkeletonHelper, SRGBColorSpace, MathUtils } from "three";
+import { degToRad } from "three/src/math/MathUtils.js";
+import { pages } from "./UI"
 
 
 // Dimensioni della pagina
@@ -8,6 +12,8 @@ const PAGE_HEIGHT = 1.71  //altezza //4:3 ASPECT RATIO
 const PAGE_DEPTH = 0.003; // spessore
 const PAGE_SEGMENTS = 30 // numero di segmenti
 const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS //larghezza del segmento
+
+    const lerpFactor = 0.05 // Costante di interpolazione lineare che controlla la velocità
 
 const pageGeometry = new BoxGeometry(
     PAGE_WIDTH,
@@ -27,15 +33,15 @@ const vertex = new Vector3();
 const skinIndexes = [] // bones
 const skinWeights = []
 
-for(let i = 0; i<position.count; i++){
+for (let i = 0; i < position.count; i++) {
     // prendo tutti i vertici
     vertex.fromBufferAttribute(position, i)
-    const x  = vertex.x //prendo la posizione x di ogni vertice
-    const skinIndex = Math.max(0, Math.floor(x/ SEGMENT_WIDTH)) // calcola la skin, in pratica influenza il bone più vicino
+    const x = vertex.x //prendo la posizione x di ogni vertice
+    const skinIndex = Math.max(0, Math.floor(x / SEGMENT_WIDTH)) // calcola la skin, in pratica influenza il bone più vicino
     const skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH // calcola il peso di ogni skin quindi l'intensità con la quale ogni bone verrà impattato (valore tra 1 e 0)
 
     skinIndexes.push(skinIndex, skinIndex + 1, 0, 0)
-    skinWeights.push(skinWeight, skinWeight + 1, 0, 0)
+    skinWeights.push(1 - skinWeight, skinWeight, 0, 0)
 }
 // In questo blocco vado ad definire le bones e a calcolare quanto ogni bone va ad impattare le bones vicine
 
@@ -61,7 +67,7 @@ const pageMaterials = [
     }),
 
     new MeshStandardMaterial({
-        color:"#111"
+        color: "#111"
     }),
 
     new MeshStandardMaterial({
@@ -72,43 +78,105 @@ const pageMaterials = [
         color: whiteColor
     }),
 
-    new MeshStandardMaterial({
-        color:"pink"
-    }),
 
-    new MeshStandardMaterial({
-        color:"blue"
-    }),
 
 ]
 // Array per costruire i materiali
 
 
+// Loader
+pages.forEach((page) => {
+    useTexture.preload(`/textures/${page.front}.jpg`)
+    useTexture.preload(`/textures/${page.back}.jpg`)
+    useTexture.preload(`/textures/book-cover-roughness.jpg`)
+})
 
-const Page = ({number, front, back, ...props}) => {
 
-    const group = useRef()
-    const skinnedMesh = useRef()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Page = ({ number, front, back, page, opened, bookClosed,   ...props }) => {
+
+    const groupRef = useRef()
+    const skinnedMeshRef = useRef()
+    const[picture, picture2, pictureRoughness] = useTexture([
+        `/textures/${front}.jpg`, 
+        `/textures/${back}.jpg`,
+        ...(number === 0 || number === pages.length - 1 
+        ? [`/textures/book-cover-roughness.jpg`]
+        : [])
+    ])
+
+
+
+    picture.colorSpace = picture2.colorSpace = SRGBColorSpace
 
     const manualSkinnedMesh = useMemo(() => {
         const bones = [];
-        for(let i = 0; i<= PAGE_SEGMENTS; i++){
-            let bone = new Bone();
-            bone.push(bone)
 
-            if( i === 0) {
+        for (let i = 0; i <= PAGE_SEGMENTS; i++) {
+            let bone = new Bone();
+            bones.push(bone)
+
+            if (i === 0) {
                 bone.position.x = 0;
-            }else {
+            } else {
                 bone.position.x = SEGMENT_WIDTH
             }
 
-            if(i > 0){
-                bones [i - 1].add(bone); //attacca la bone alla bone precedente
+            if (i > 0) {
+                bones[i - 1].add(bone); //attacca la bone alla bone precedente
             }
         }
 
         const skeleton = new Skeleton(bones)
-        const materials = pageMaterials;
+
+
+        // Materiali
+        const materials = [...pageMaterials,
+        new MeshStandardMaterial({
+            color: whiteColor,
+            map: picture,
+            ...(number === 0
+                ? {
+                    roughnessMap: pictureRoughness,
+                }
+                : {
+                    roughness: 0.1
+                }
+            )
+        }),
+
+        new MeshStandardMaterial({
+            color: whiteColor,
+            map: picture2,
+            ...(number === pages.length - 1
+                ? {
+                    roughnessMap: pictureRoughness,
+                }
+                : {
+                    roughness: 0.1
+                }
+            )
+        })
+
+        ];
+        // Materiali
+
         const mesh = new SkinnedMesh(pageGeometry, materials);
         mesh.castShadow = true
         mesh.receiveShadow = true
@@ -120,14 +188,35 @@ const Page = ({number, front, back, ...props}) => {
     }, [])
 
 
+
+    // ANIMAZIONI
+    useFrame(() => {
+
+        // Guardian
+        if(!skinnedMeshRef.current){
+            return
+        }
+
+        let targetRotation = opened ? -Math.PI /2 : Math.PI /2
+        if(!bookClosed){
+            targetRotation += degToRad(number * 0.8)
+        }
+
+        const bones = skinnedMeshRef.current.skeleton.bones
+
+        bones[0].rotation.y = MathUtils.lerp(bones[0].rotation.y, targetRotation, lerpFactor)
+    })
+
     return (
-    <group {...props} ref={group}>
-        <mesh scale={0.1}>
-            <primitive object={pageGeometry} attach={"geometry"}/>
-            <meshBasicMaterial color={"red"}/>
-        </mesh>
-    </group>
-)
+        <group {...props} ref={groupRef}>
+            <primitive 
+            object={manualSkinnedMesh} 
+            ref={skinnedMeshRef} 
+            position-z = {-number * PAGE_DEPTH + page * PAGE_DEPTH}
+            
+            />
+        </group>
+    )
 }
 
 export default Page
